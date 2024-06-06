1e96889a5af8d41ff7befb843f5eecb76dbd2b5e
pratik-senapati <senapatipratik2019@gmail.com>
2024-06-06 15:07:38 +0530

    add threading for encrypt and decrypt

--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include "head.h"
+#include<pthread.h>
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* enc_thread()
+{
+    
+
+    pthread_mutex_lock(&mutex);
+    encrypt();
+    printf("Encrypted\n");
+    pthread_mutex_unlock(&mutex);
+}
+
+void* dec_thread()
+{
+    
+
+    pthread_mutex_lock(&mutex);
+    decrypt();
+    printf("Decrypted\n");
+    pthread_mutex_unlock(&mutex);
+}
 
 int main()
 {
-    printf("Start\n");
-    // fflush(stdout);
-    encrypt();
+    pthread_t t1;
+    pthread_t t2;
 
-    sleep(2);
-    printf("Encrypted\n");
+    pthread_mutex_init(&mutex, NULL);
 
-    decrypt();
-    printf("Decrypted\n");
+    printf("Start");
+
+    pthread_create(&t1, NULL, enc_thread, NULL);
+    pthread_create(&t2, NULL, dec_thread, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&mutex);
 
 
     return 2;
